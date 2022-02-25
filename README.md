07555e8cbf7cbd2de60be8a8b3ea654bcace6602
alew <adam.lewandowski@inetum.com>
2022-02-25 22:28:26 +0100

    Add simple Tests

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # weather_app
 
-Add description how to run the app
+Build a “Worldwide Windsurfer’s Weather Service” application meeting the requirements listed below.
+
+1.Use the Weatherbit Forecast API(https://www.weatherbit.io/api/weather-forecast-16-day) as the weather data source.
+
+2. Expose a REST API, where the argument is a day (yyyy-mm-dd date format) and return value is one of following locations:
+
+Jastarnia (Poland)
+Bridgetown (Barbados)
+Fortaleza (Brazil)
+Pissouri (Cyprus)
+Le Morne (Mauritius)
+
+depending on which place offers better windsurfing conditions on that day in the 16 forecast day range. Apart from returning the name of the location, the response should also include weather conditions (at least average temperature - Celcius, wind speed - m/s) for the location on that day.
+
+
+3. The list of windsurfing locations (including geographical coordinates) should be embedded in the application in a way that allows for extensions at a later stage. Note: There is no API requirement for creating or editing the locations.
+
+4. The best location selection criteria are:
+   If the wind speed is not within <5; 18> (m/s) and the temperature is not in the range <5; 35> (°C), the location is not suitable for windsurfing. However, if they are in these ranges, then the best location is determined by the highest value calculated from the following formula:
+   v * 3 + temp
+   where v is the wind speed in m/s on a given day, and temp is an average forecasted temperature for a given day in Celsius, respectively - you can obtain these parameters from the “data” key in Weatherbit API’s response.
+   If none of the locations meets the above criteria, the application does not return any.
+
+5. The application is written in Java 8 or higher and uses Spring Boot. Frontend is not required.
+
+6. It has a gradle or maven building mechanism and a README file describing at least the procedure for building and running the application.
+
+7. Exceptional scenarios or requirements not specified above should be handled at your discretion. 
--- src/test/java/com/example/weather_app/service/SurfingSpotProviderTest.java
+++ src/test/java/com/example/weather_app/service/SurfingSpotProviderTest.java
@@ -1,11 +1,97 @@
 package com.example.weather_app.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.weather_app.model.Forecast;
+import com.example.weather_app.model.Location;
+import com.example.weather_app.model.SurfingSpot;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.util.Date;
+import java.util.stream.Stream;
+
+import static com.example.weather_app.model.Location.JASTARNIA;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.of;
+import static org.mockito.Mockito.*;
 
 class SurfingSpotProviderTest {
 
+    @Mock
+    private ForecastProvider forecastProvider;
 
-    // add tests
+    @InjectMocks
+    private SurfingSpotProvider surfingSpotProvider;
 
+    @ParameterizedTest
+    @MethodSource("goodConditionsData")
+    void findBestSpot_goodDateAndConditions_ReturnsBestSurfingSpot(String temperatureJastarnia, String windSpeedJastarnia, String temperatureForRest, String windSpeedForRest) throws ParseException {
+        //given
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = format.format(new Date());
+        Date date = format.parse("2022-02-25");
+        Forecast forecastJastarnia = new Forecast(temperatureJastarnia, windSpeedJastarnia);
+        Forecast forecastForRest = new Forecast(temperatureForRest, windSpeedForRest);
+        SurfingSpot bestSurfingSpot = new SurfingSpot(JASTARNIA, temperatureJastarnia, windSpeedJastarnia);
+        when(forecastProvider.getForecast(JASTARNIA, date)).thenReturn(forecastJastarnia);
+        when(forecastProvider.getForecast(any(Location.class), date)).thenReturn(forecastForRest);
+        //when
+        SurfingSpot bestSpot = surfingSpotProvider.findBestSpot(date);
+        //then
+        assertThat(bestSpot).isEqualTo(bestSurfingSpot);
+        verify(forecastProvider, times(5)).getForecast(any(Location.class), any(Date.class));
+        verifyNoMoreInteractions(forecastProvider);
+    }
 
+    private static Stream<Arguments> goodConditionsData() {
+        return Stream.of(
+                of("35", "18.0", 5, "5", "5.0"),
+                of("5", "5.0", 5, "4", "4.9"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("badConditionsData")
+    void findBestSpot_goodDateBadConditionsForSurfing_DoesNotReturnAny(String temperatureJastarnia, String windSpeedJastarnia, String temperatureForRest, String windSpeedForRest) throws ParseException {
+        //given
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = format.format(new Date());
+        Date date = format.parse("2022-02-25");
+        Forecast forecastJastarnia = new Forecast(temperatureJastarnia, windSpeedJastarnia);
+        Forecast forecastForRest = new Forecast(temperatureForRest, windSpeedForRest);
+        SurfingSpot bestSurfingSpot = new SurfingSpot(null, null, null);
+        when(forecastProvider.getForecast(JASTARNIA, date)).thenReturn(forecastJastarnia);
+        when(forecastProvider.getForecast(any(Location.class), date)).thenReturn(forecastForRest);
+        //when
+        SurfingSpot bestSpot = surfingSpotProvider.findBestSpot(date);
+        //then
+        assertThat(bestSpot).isEqualTo(bestSurfingSpot);
+        verify(forecastProvider, times(5)).getForecast(any(Location.class), any(Date.class));
+        verifyNoMoreInteractions(forecastProvider);
+    }
+
+    private static Stream<Arguments> badConditionsData() {
+        return Stream.of(
+                of("36", "8.4", 5, "4", "5.9"),
+                of("25", "18.1", 5, "16", "4.9"));
+    }
+
+    @Test
+    void findBestSpot_badDate_DoesNotReturnAny() throws ParseException, DateTimeException {
+        //given
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = format.format(new Date());
+        Date date = format.parse("2025-13-35");
+        assertThatThrownBy(() -> surfingSpotProvider.findBestSpot(date))
+                //then
+                .isInstanceOf(DateTimeException.class)
+                .hasMessageContaining("Incorrect date!");
+        verify(forecastProvider, never()).getForecast(any(Location.class), any(Date.class));
+    }
 }
